2607bb9ab5ca9ef6c1b526091b74347e770410a3
lburgazzoli <lburgazzoli@gmail.com>
2013-03-05 15:15:23 +0100

    pm

--- cm-inmemory/src/main/java/lb/examples/karaf/cm/InMemoryPersistenceManager.java
+++ cm-inmemory/src/main/java/lb/examples/karaf/cm/InMemoryPersistenceManager.java
@@ -16,13 +16,19 @@
  */
 package lb.examples.karaf.cm;
 
+import com.google.common.collect.Maps;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.felix.cm.PersistenceManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Dictionary;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
 
 /**
  *
@@ -30,31 +36,62 @@ import java.util.Enumeration;
 public class InMemoryPersistenceManager implements PersistenceManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(InMemoryPersistenceManager.class);
 
+    private Map<String,Map<Object,Object>> m_data;
+
+    public InMemoryPersistenceManager() {
+        m_data = Maps.newHashMap();
+        m_data.put("lb.examples.karaf",new HashMap<Object,Object>());
+        m_data.put("lb.examples.karaf.jpa",new HashMap<Object,Object>());
+        m_data.put("lb.examples.karaf.data",new HashMap<Object,Object>());
+    }
+
     @Override
     public boolean exists(String pid) {
-        LOGGER.debug("exists : {}",pid);
-        return false;
+        LOGGER.debug("exists {} ({})",pid,StringUtils.startsWith(pid,"lb.examples.karaf"));
+        return StringUtils.startsWith(pid, "lb.examples.karaf");
     }
 
     @Override
     public Dictionary load(String pid) throws IOException {
-        LOGGER.debug("load : {}",pid);
-        return null;
+        LOGGER.debug("load {}",pid);
+        if(!m_data.containsKey(pid)) {
+            m_data.put(pid,new HashMap<Object,Object>());
+        }
+
+        Map<Object,Object> ht = m_data.get(pid);
+        Hashtable<Object,Object> dt = new Hashtable<Object,Object>(ht);
+
+        LOGGER.debug("load {}, {}",pid,dt.size());
+        LOGGER.debug("....");
+
+        return dt;
     }
 
     @Override
     public Enumeration getDictionaries() throws IOException {
         LOGGER.debug("getDictionaries");
-        return null;
+        return Collections.enumeration(m_data.values());
     }
 
     @Override
     public void store(String pid, Dictionary properties) throws IOException {
         LOGGER.debug("store : {}",pid);
+        if(!m_data.containsKey(pid)) {
+            Map<Object,Object> ht = Maps.newHashMap();
+            Enumeration e = properties.keys();
+            while(e.hasMoreElements()){
+                Object key = e.nextElement();
+                ht.put(key,properties.get(key));
+                LOGGER.debug("pid={], k={}, v={}",pid,key,properties.get(key));
+            }
+
+            m_data.put(pid,ht);
+        }
     }
 
     @Override
     public void delete(String pid) throws IOException {
         LOGGER.debug("delete : {}",pid);
+        m_data.remove(pid);
     }
 }
