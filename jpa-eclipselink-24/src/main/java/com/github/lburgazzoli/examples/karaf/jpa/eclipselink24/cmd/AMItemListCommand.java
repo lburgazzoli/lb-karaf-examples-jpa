11f27bef82e695f8b1e30b7ac154cad5dd90aedf
lburgazzoli <lburgazzoli@gmail.com>
2014-09-14 18:10:22 +0200

    start working on eclipse 2.5.x

---
+++ jpa-eclipselink-24/src/main/java/com/github/lburgazzoli/examples/karaf/jpa/eclipselink24/cmd/AMItemListCommand.java
@@ -0,0 +1,36 @@
+/**
+ *
+ * Copyright 2013 lb
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.lburgazzoli.examples.karaf.jpa.eclipselink24.cmd;
+
+import com.github.lburgazzoli.examples.karaf.jpa.eclipselink24.data.Item;
+import org.apache.felix.gogo.commands.Command;
+
+/**
+ *
+ */
+@Command(scope = "item", name = "eclipselink-am-list", description = "Lists all items (AM)")
+public class AMItemListCommand extends AbstractItemCommand {
+
+    @Override
+    public Object doExecute() throws Exception {
+        for (Item item : getDataService().getAll()) {
+            System.out.println(item.getName() + ", " + item.getDescription());
+        }
+
+        return null;
+    }
+}
---
+++ jpa-eclipselink-25/src/main/java/com/github/lburgazzoli/examples/karaf/jpa/eclipselink25/data/ApplicationManagedDataService.java
@@ -0,0 +1,77 @@
+/**
+ *
+ * Copyright 2013 lb
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.lburgazzoli.examples.karaf.jpa.eclipselink25.data;
+
+import com.github.lburgazzoli.examples.karaf.jpa.commons.data.IDataService;
+import com.google.common.collect.Lists;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import java.util.Collection;
+
+/**
+ *
+ */
+public class ApplicationManagedDataService implements IDataService<Item> {
+    private EntityManagerFactory emf;
+
+    /**
+     * c-tor
+     */
+    public ApplicationManagedDataService() {
+        this.emf = null;
+    }
+
+    /**
+     * @param emf the EntityManagerFactory
+     */
+    public void setEntityManagerFactory(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+
+    @Override
+    public Collection<Item> getAll() {
+        EntityManager    em  = emf.createEntityManager();
+        Collection<Item> ret = Lists.newArrayList();
+
+        try {
+            ret = em.createQuery("select i from Item i", Item.class).getResultList();
+        } finally {
+            em.close();
+        }
+
+        return ret;
+    }
+
+    @Override
+    public void add(Item item) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.persist(item);
+            em.getTransaction().commit();
+        } catch(Exception e) {
+            if(em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+
+            System.out.println("Exception: " + e.getMessage());
+        } finally {
+            em.close();
+        }
+    }
+}
